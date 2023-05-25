232897a7e7d6d4a542de914c09f777e054ee8fbf
chopper2718 <ivm9.im@gmail.com>
2023-05-25 13:37:47 +0000

    update

--- fish/fish/config.fish
+++
@@ -1,9 +0,0 @@
-set fish_greeting
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-function fish_prompt
-    echo -en '\033[36m['(prompt_pwd)']\033[0m\033[35m λ '
-end
